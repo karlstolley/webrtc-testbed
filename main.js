bb5cece82991452ca764f16c178b1324dd5ab2b2
Karl Stolley <karl.stolley@gmail.com>
2022-11-04 13:23:53 -0500

    Improve testbed starter

--- main.js
+++ main.js
@@ -5,18 +5,16 @@ const pc = {
   passive: new RTCPeerConnection(null)
 }
 
-pc.active.onicecandidate = event => pc.passive.addIceCandidate(event.candidate);
-pc.passive.onicecandidate = event => pc.active.addIceCandidate(event.candidate);
-
-pc.active.oniceconnectionstatechange = event => { console.log(pc.active.iceConnectionState); }
-pc.passive.oniceconnectionstatechange = event => { console.log(pc.passive.iceConnectionState); }
-
-console.log(`Passive connection state: ${pc.passive.connectionState}`);
+// ICE events
+pc.active.onicecandidate = ({candidate}) => pc.passive.addIceCandidate(candidate);
+pc.passive.onicecandidate = ({candidate}) => pc.active.addIceCandidate(candidate);
+pc.active.oniceconnectionstatechange = event => { console.log(`Active ICE: ${pc.active.iceConnectionState}`); }
+pc.passive.oniceconnectionstatechange = event => { console.log(`Passive ICE: ${pc.passive.iceConnectionState}`); }
 
 function negotiate(a, b) {
   return a.createOffer()
     .then(function(offer) {
-      console.log(`Offer: ${offer}`);
+      console.log(`Offer: ${JSON.stringify(offer)}`);
       return a.setLocalDescription(offer);
     }).then(function() {
       return b.setRemoteDescription(a.localDescription);
@@ -29,31 +27,23 @@ function negotiate(a, b) {
   });
 }
 
-const dc = pc.active.createDataChannel('up', { protocol: 'strings', negotiated: true, id: 101 });
-const dc = pc.passive.createDataChannel('up', { protocol: 'strings', negotiated: true, id: 101 });
-
-function registerDataChannelCallbacks(dc) {
-  dc.onopen = (event) => {
-    dc.send('Hi there!');
+function registerDataChannelCallbacks(dc, role) {
+  dc.onopen = () => {
+    dc.send(`Hi there from the ${role} role`);
   }
   dc.onmessage = event => {
     console.log(event.data);
   }
 }
-try {
-  dc.send('This is going out RIGHT AWAY.');
-} catch(e) {
-  console.error(e);
-}
 
-
-registerDataChannelCallbacks(dc);
-
-pc.passive.ondatachannel = event => {
-  const dc = event.channel;
-  console.log('Channel properties', dc);
-  registerDataChannelCallbacks(dc);
-  dc.send('Hello again from me');
-}
+// This will be enough to always establish a peer connection
+const dc = pc.active.createDataChannel('up', { protocol: 'strings' });
+registerDataChannelCallbacks(dc, 'active');
+// Register the same call backs on the passive peer
+pc.passive.ondatachannel = ({channel}) => {
+  registerDataChannelCallbacks(channel, 'passive');
+};
 
 negotiate(pc.active, pc.passive);
+
+// Now do whatever you like with a peer connection established...
