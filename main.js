7a14345fdb79f8bc041a42ccffda13321326906b
Karl Stolley <karl@stolley.dev>
2023-06-19 11:49:55 -0500

    Mock perfect-negotiation rollback and log state

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ const pc = {
 const ndca = pc.active.createDataChannel('negotiated', { negotiated: true, id: 5});
 const ndcb = pc.passive.createDataChannel('negotiated', { negotiated: true, id: 5});
 
-registerDataChannelCallbacks(ndca, 'active - negotiated');
-registerDataChannelCallbacks(ndcb, 'passive - negotiated');
+registerDataChannelCallbacks(ndca, 'active - negotiated data channel');
+registerDataChannelCallbacks(ndcb, 'passive - negotiated data channel');
 
 // Register callbacks on ICE events
 pc.active.onicecandidate = ({candidate}) => pc.passive.addIceCandidate(candidate);
@@ -18,7 +18,12 @@ pc.active.oniceconnectionstatechange = () => { console.log(`Active ICE: ${pc.act
 pc.passive.oniceconnectionstatechange = () => { console.log(`Passive ICE: ${pc.passive.iceConnectionState}`); }
 
 // Register callback on negotiation-needed event
-pc.active.onnegotiationneeded = () => { negotiate(pc.active, pc.passive); }
+pc.active.onnegotiationneeded = () => { perfectly_negotiate(pc.active, pc.passive); }
+
+// Log data-channel readyState on each *connection* state change
+pc.active.onconnectionstatechange = (event) => { log_dc_ready_states(event); }
+pc.passive.onconnectionstatechange = (event) => { log_dc_ready_states(event); }
+
 
 function negotiate(a, b) {
   return a.createOffer()
@@ -34,6 +39,41 @@ function negotiate(a, b) {
     }).then(function() {
       return a.setRemoteDescription(b.localDescription);
   });
+}
+
+function log_dc_ready_states(event) {
+  setTimeout(function() {
+  console.log(
+    event.target.connectionState,
+    `>>> Active DC readyState: ${ndca.readyState}\n`,
+    `>>> Passive DC readyState: ${ndcb.readyState}`,
+  )}, 50);
+}
+
+function perfectly_negotiate(a, b) {
+  // Put B in a glare state for rollback
+  b.createOffer().then(function(offer) {
+    return b.setLocalDescription(offer);
+  }).then(function() {
+    console.log('Negotiate with rollback...');
+  });
+  return a.createOffer()
+    .then(function(offer) {
+      console.log(`Offer: ${JSON.stringify(offer)}`);
+      return a.setLocalDescription(offer);
+    }).then(function() {
+      // This is where things break down, I believe;
+      // When b (pc.passive) rolls back its own offer here,
+      // its negotiated data channel hangs in the "connecting"
+      // state
+      return b.setRemoteDescription(a.localDescription);
+    }).then(function() {
+      return b.createAnswer();
+    }).then(function(answer) {
+      return b.setLocalDescription(answer);
+    }).then(function() {
+      return a.setRemoteDescription(b.localDescription);
+    });
 }
 
 function registerDataChannelCallbacks(dc, role) {
